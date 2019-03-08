c84b543e5da2fb36ebc0a64dfde27934e6e997eb
Parzival Roethlein <parzival.roethlein@reelfx.com>
2019-03-08 13:34:10 -0500

    placeholders for more scalable versions of maya utility nodes

---
+++ prmaya/plugins/prBlendColors.py
@@ -0,0 +1,13 @@
+"""
+bc.input[0].color1.color1R
+bc.input[0].color1.color1G
+bc.input[0].color1.color1B
+
+bc.input[0].color2.color2R
+bc.input[0].color2.color2G
+bc.input[0].color2.color2B
+
+bc.output[0].outputR
+bc.output[0].outputG
+bc.output[0].outputB
+"""
