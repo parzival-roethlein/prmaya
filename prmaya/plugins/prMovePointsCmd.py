f21b8b779131858fc61107af6bf339ee2a25d405
Parzival Roethlein <pa.roethlein@gmail.com>
2020-01-16 23:23:48 -0500

    optimized

--- prmaya/plugins/prMovePointsCmd.py
+++ prmaya/plugins/prMovePointsCmd.py
@@ -23,6 +23,8 @@ movePoints(mesh=cube, deltas={5: om.MVector([0, 0.5, 0])})
 """
 
 import sys
+from itertools import izip
+
 
 import maya.cmds as mc
 import maya.api.OpenMaya as om
@@ -33,78 +35,49 @@ def movePoints(mesh, deltas, space=om.MSpace.kObject):
     simplified interface to run PrMovePointsCmd
 
     :param mesh: mesh shape name
-    :param deltas: see PrMovePointsCmd.convert_deltas_to_cmd_args
+    :param deltas: {vertexId: MVector(), ...}
     :param space: om.kSpace.kObject
     :return:
     """
-    ids, vectors = PrMovePointsCmd.convert_deltas_to_cmd_args(deltas)
-    mc.prMovePointsCmd(mesh, ids, vectors, space)
+    mc.prMovePointsCmd(mesh, space, deltas.keys(), deltas.values())
 
 
 class PrMovePointsCmd(om.MPxCommand):
 
     PLUGIN_NAME = 'prMovePointsCmd'
 
-    @staticmethod
-    def convert_deltas_to_cmd_args(deltas):
-        """
-        convert different types of given deltas to flat lists (MPxCommand arguments type limitation)
-        :param deltas: indexed vector dict // list of vectors // MVectorArray // MPointArray
-        :return: (vertexIds, flatVectorList) : ([0, 1, ...], [1.0, 0.0, ...])
-        """
-        vertexIds = []
-        flatVectorList = []
-
-        if isinstance(deltas, dict):
-            for pointId, vec in deltas.iteritems():
-                vertexIds.append(pointId)
-                flatVectorList += [vec[0], vec[1], vec[2]]
-
-        if isinstance(deltas, list) or isinstance(deltas, om.MVectorArray) or isinstance(deltas, om.MPointArray):
-            vertexIds = list(range(len(deltas)))
-            for vec in deltas:
-                flatVectorList += [vec[0], vec[1], vec[2]]
-        return vertexIds, flatVectorList
-
-    @staticmethod
-    def convert_cmd_args_to_deltas(vertexIds, vectors):
-        """
-        convert a list of vertex ids and a flat vector list to an indexed MVector dict
-        :param vertexIds: [0, 1, ...]
-        :param vectors: [1.0, 0.0, ...]
-        :return:
-        """
-        deltas = {}
-        for x, vertexId in enumerate(vertexIds):
-            deltas[vertexId] = om.MVector(vectors[x*3], vectors[x*3+1], vectors[x*3+2])
-        return deltas
-
     def addDeltas(self, undoCall=False):
-        selection = om.MSelectionList()
-        selection.add(self.mesh)
-        vertex_iter = om.MItMeshVertex(selection.getDagPath(0))
         for vertexId, delta in self.deltas.iteritems():
-            vertex_iter.setIndex(vertexId)
-            position = vertex_iter.position(space=self.space)
+            self.vertexIterator.setIndex(vertexId)
+            position = self.vertexIterator.position(space=self.space)
             if undoCall:
                 position -= delta
             else:
                 position += delta
-            vertex_iter.setPosition(position, space=self.space)
+            self.vertexIterator.setPosition(position, space=self.space)
 
     def __init__(self):
         om.MPxCommand.__init__(self)
-        self.mesh = None
+        self.vertexIterator = None
         self.deltas = None
         self.space = None
         self.undo_positions = None
 
     def doIt(self, args):
-        self.mesh = args.asString(0)
-        vertexIds = args.asIntArray(1)
-        vectors = args.asDoubleArray(2)
-        self.space = args.asInt(3)
-        self.deltas = PrMovePointsCmd.convert_cmd_args_to_deltas(vertexIds=vertexIds, vectors=vectors)
+        """
+        :param args: meshName, space, vertexIdList, vector_1, vector_2, ... vector_n
+        :return:
+        """
+        mesh = args.asString(0)
+        selection = om.MSelectionList()
+        selection.add(mesh)
+        self.vertexIterator = om.MItMeshVertex(selection.getDagPath(0))
+        self.space = args.asInt(1)
+        vertexIds = args.asIntArray(2)
+        if len(args)-3 != len(vertexIds):
+            raise ValueError('vectorIds size: {0} does not match MVector count: {1}'.format(len(vertexIds), len(args)-3))
+        self.deltas = {vert: args.asVector(vec) for vert, vec in izip(vertexIds, xrange(3, len(args)))}
+
         self.redoIt()
 
     def redoIt(self):
