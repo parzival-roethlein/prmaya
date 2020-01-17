05a6d91300effd1d78b12152527ad1c5b143aa27
Parzival Roethlein <parzival.roethlein@reelfx.com>
2020-01-17 18:15:41 -0500

    new prAverageDeltasCmd to maybe improve performance of prDeformPaint

---
+++ prmaya/plugins/prAverageDeltasCmd.py
@@ -0,0 +1,113 @@
+"""
+
+
+USAGE:
+
+"""
+
+import sys
+from itertools import izip
+
+
+import maya.api.OpenMaya as om
+
+
+class prAverageDeltasCmd(om.MPxCommand):
+
+    PLUGIN_NAME = 'prAverageDeltasCmd'
+
+    def addDeltas(self, undoCall=False):
+        for vertexId, delta in izip(self.vertexIds, self.deltas):
+            self.drivenIter.setIndex(vertexId)
+            position = self.drivenIter.position(self.space)
+            if undoCall:
+                position -= delta
+            else:
+                position += delta
+            self.drivenIter.setPosition(position, self.space)
+
+    def __init__(self):
+        om.MPxCommand.__init__(self)
+        self.drivenIter = None
+        self.vertexIds = None
+        self.deltas = None
+        self.space = None
+
+    def doIt(self, args):
+        """
+        :param args: baseMesh, drivenMesh, om.MSpace.kObject, [vertexId_1, ...], [weight_1, ...], weight
+        :return:
+        """
+        baseMesh = args.asString(0)
+        drivenMesh = args.asString(1)
+        space = args.asInt(2)
+        self.space = space
+        self.vertexIds = args.asIntArray(3)
+        vertexWeights = args.asDoubleArray(4)
+        weight = args.asFloat(5)
+        if len(self.vertexIds) != len(vertexWeights):
+            raise ValueError('len(vertexIds) != len(vertexWeights) // {0} != {1}'.format(
+                             len(self.vertexIds), len(vertexWeights)))
+        selection = om.MSelectionList()
+        selection.add(baseMesh)
+        baseIter = om.MItMeshVertex(selection.getDagPath(0))
+        selection.add(drivenMesh)
+        self.drivenIter = om.MItMeshVertex(selection.getDagPath(1))
+        self.deltas = om.MVectorArray()
+
+        # calculate deformation delta
+        for vertexId, vertexWeight in izip(self.vertexIds, vertexWeights):
+            self.drivenIter.setIndex(vertexId)
+
+            # get average delta
+            averageDelta = om.MVector()
+            neighborVtxIds = self.drivenIter.getConnectedVertices()
+            for neighborVtxId in neighborVtxIds:
+                baseIter.setIndex(neighborVtxId)
+                self.drivenIter.setIndex(neighborVtxId)
+                averageDelta += self.drivenIter.position(space) - baseIter.position(space)
+            averageDelta *= 1.0/len(neighborVtxIds)
+
+            # final delta
+            self.drivenIter.setIndex(vertexId)
+            baseIter.setIndex(vertexId)
+            self.deltas.append(
+                (baseIter.position(space) + averageDelta - self.drivenIter.position(space)) * vertexWeight * weight
+            )
+        self.redoIt()
+
+    def redoIt(self):
+        self.addDeltas()
+
+    def undoIt(self):
+        self.addDeltas(undoCall=True)
+
+    @staticmethod
+    def creator():
+        return prAverageDeltasCmd()
+
+    def isUndoable(self):
+        return True
+
+
+def maya_useNewAPI():
+    pass
+
+
+def initializePlugin(obj):
+    plugin = om.MFnPlugin(obj, "Parzival Roethlein", "0.0.1", "Any")
+    try:
+        plugin.registerCommand(prAverageDeltasCmd.PLUGIN_NAME, prAverageDeltasCmd.creator)
+    except:
+        sys.stderr.write("Failed to register command\n")
+        raise
+
+
+def uninitializePlugin(obj):
+    plugin = om.MFnPlugin(obj)
+    try:
+        plugin.deregisterCommand(prAverageDeltasCmd.PLUGIN_NAME)
+    except:
+        sys.stderr.write("Failed to deregister command\n")
+        raise
+
---
+++ test/plugins/test_prAverageDeltasCmd.py
@@ -0,0 +1,16 @@
+
+import maya.cmds as mc
+import maya.api.OpenMaya as om
+from prmaya.plugins import prAverageDeltasCmd
+reload(prAverageDeltasCmd)
+
+mc.file(newFile=True, force=True)
+mc.unloadPlugin('prAverageDeltasCmd.py')
+# mc.loadPlugin('C:/Users/paz/Documents/git/prmaya/prmaya/plugins/prAverageDeltasCmd.py')
+# mc.file('C:/Users/paz/Documents/git/prmaya/test/plugins/test_prAverageDeltasCmd.ma', open=True, force=True)
+mc.loadPlugin('/home/prthlein/private/code/prmaya/prmaya/plugins/prAverageDeltasCmd.py')
+mc.file('/home/prthlein/private/code/prmaya/test/plugins/test_prAverageDeltasCmd.ma', open=True, force=True)
+
+mc.prAverageDeltasCmd('base', 'driven', om.MSpace.kObject, [213, 216], [1.0, 1.0], 1.0)
+
+mc.prAverageDeltasCmd('base', 'driven', om.MSpace.kObject, [216], [1.0], 1.0)
