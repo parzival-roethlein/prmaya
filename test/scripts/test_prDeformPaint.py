154eb5aa946776087fb7f61e909f36670777a357
Parzival Roethlein <pa.roethlein@gmail.com>
2020-01-22 01:07:30 -0500

    deformPaint latest home test update

--- test/scripts/test_prDeformPaint.py
+++ test/scripts/test_prDeformPaint.py
@@ -1,13 +1,17 @@
 import maya.cmds as mc
 import prDeformPaint;reload(prDeformPaint)
-prDeformPaint.Ui()
+ui = prDeformPaint.Ui()
 
-testFile = '/home/prthlein/private/code/prmaya/test/scripts/test_prDeformPaint.ma'
+#testFile = '/home/prthlein/private/code/prmaya/test/scripts/test_prDeformPaint.ma'
+testFile = r'C:\Users\paz\Documents\git\prmaya\test\scripts\test_prDeformPaint.ma'
 
 mc.file(testFile, open=True, force=True)
 mc.file(rename=testFile.replace('.ma', 'TEMP.ma'))
 mc.file(renameToSave=True)
 prDeformPaint.reinitializeMaya()
+
+mc.select('half')
+ui.enterTool()
 
 #prDeformPaint.initializeMaya('/home/prthlein/private/code/prmaya/prmaya/plugins/prMovePointsCmd.py',
 #                             '/home/prthlein/private/code/prmaya/prmaya/scripts/prDeformPaintBrush.mel')
